d74f6075e6de4f5125f5dc187a5134414c5a32e1
Tejun Heo <tj@kernel.org>
2011-01-26 12:12:50 +0100

    cpufreq: use system_wq instead of dedicated workqueues
    With cmwq, there's no reason for cpufreq drivers to use separate
    workqueues.  Remove the dedicated workqueues from cpufreq_conservative
    and cpufreq_ondemand and use system_wq instead.  The work items are
    already sync canceled on stop, so it's already guaranteed that no work
    is running on module exit.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: Dave Jones <davej@redhat.com>
    Cc: cpufreq@vger.kernel.org
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_ondemand.c

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -105,8 +105,6 @@ static unsigned int dbs_enable;	/* number of CPUs using this policy */
  */
 static DEFINE_MUTEX(dbs_mutex);
 
-static struct workqueue_struct	*kondemand_wq;
-
 static struct dbs_tuners {
 	unsigned int sampling_rate;
 	unsigned int up_threshold;
@@ -671,7 +669,7 @@ static void do_dbs_timer(struct work_struct *work)
 		__cpufreq_driver_target(dbs_info->cur_policy,
 			dbs_info->freq_lo, CPUFREQ_RELATION_H);
 	}
-	queue_delayed_work_on(cpu, kondemand_wq, &dbs_info->work, delay);
+	schedule_delayed_work_on(cpu, &dbs_info->work, delay);
 	mutex_unlock(&dbs_info->timer_mutex);
 }
 
@@ -685,8 +683,7 @@ static inline void dbs_timer_init(struct cpu_dbs_info_s *dbs_info)
 
 	dbs_info->sample_type = DBS_NORMAL_SAMPLE;
 	INIT_DELAYED_WORK_DEFERRABLE(&dbs_info->work, do_dbs_timer);
-	queue_delayed_work_on(dbs_info->cpu, kondemand_wq, &dbs_info->work,
-		delay);
+	schedule_delayed_work_on(dbs_info->cpu, &dbs_info->work, delay);
 }
 
 static inline void dbs_timer_exit(struct cpu_dbs_info_s *dbs_info)
@@ -713,7 +710,6 @@ static int ondemand_suspend(struct platform_device *dev, pm_message_t state)
 	this_dbs_info = &per_cpu(od_cpu_dbs_info, cpu);
 
 	dbs_timer_exit(this_dbs_info);
-	flush_workqueue(kondemand_wq);
 
 	/* force lowest opp available */
 	__cpufreq_driver_target(this_dbs_info->cur_policy, 0,
@@ -872,7 +868,6 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 
 static int __init cpufreq_gov_dbs_init(void)
 {
-	int err;
 	cputime64_t wall;
 	u64 idle_time;
 	int cpu = get_cpu();
@@ -896,35 +891,13 @@ static int __init cpufreq_gov_dbs_init(void)
 			MIN_SAMPLING_RATE_RATIO * jiffies_to_usecs(10);
 	}
 
-	kondemand_wq = create_workqueue("kondemand");
-	if (!kondemand_wq) {
-		printk(KERN_ERR "Creation of kondemand failed\n");
-		return -EFAULT;
-	}
-	err = cpufreq_register_governor(&cpufreq_gov_ondemand);
-	if (err)
-		goto err2;
-
-	err = platform_driver_register(&ondemand_dummy_driver);
-	if (err)
-		goto err1;
-
-	return 0;
-
-err1:
-	cpufreq_unregister_governor(&cpufreq_gov_ondemand);
-
-err2:
-	destroy_workqueue(kondemand_wq);
-
-	return err;
+	return cpufreq_register_governor(&cpufreq_gov_ondemand);
 }
 
 static void __exit cpufreq_gov_dbs_exit(void)
 {
 	platform_driver_unregister(&ondemand_dummy_driver);
 	cpufreq_unregister_governor(&cpufreq_gov_ondemand);
-	destroy_workqueue(kondemand_wq);
 }
 
 
