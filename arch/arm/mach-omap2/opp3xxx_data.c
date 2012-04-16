461745d1f743c1a8184721076632210a041b3e30
Ambresh K <ambresh@ti.com>
2012-04-16 20:15:06 +0530

    OMAP3: DVFS: Using the l3_ick for core in OPP Table
    Instead of using dpll3_m2_ck to scale core between opp50 and  opp100,
    use l3_ick clock reference in table.
    
    Change-Id: Ifb1762a84c9db19297c1d910b97918d504f355d1
    Tested-by: Deepak Muddegowda <x0171695@ti.com>
    Signed-off-by: Deepak Muddegowda <x0171695@ti.com>
    Signed-off-by: Ambresh K <ambresh@ti.com>

--- arch/arm/mach-omap2/opp3xxx_data.c
+++ arch/arm/mach-omap2/opp3xxx_data.c
@@ -159,9 +159,11 @@ static struct omap_opp_def __initdata omap36xx_opp_def_list[] = {
 				1000000000, OMAP3630_VDD_MPU_OPP1G_UV),
 
 	/* L3 OPP1 - OPP50 */
-	OPP_INITIALIZER("l3_main", "dpll3_ck", "core", true, 100000000, OMAP3630_VDD_CORE_OPP50_UV),
+	OPP_INITIALIZER("l3_main", "l3_ick", "core", true,
+				100000000, OMAP3630_VDD_CORE_OPP50_UV),
 	/* L3 OPP2 - OPP100, OPP-Turbo, OPP-SB */
-	OPP_INITIALIZER("l3_main", "dpll3_ck", "core", true, 200000000, OMAP3630_VDD_CORE_OPP100_UV),
+	OPP_INITIALIZER("l3_main", "l3_ick", "core", true,
+				200000000, OMAP3630_VDD_CORE_OPP100_UV),
 
 	/* DSP OPP1 - OPP50 */
 	OPP_INITIALIZER("iva", "dpll2_ck", "mpu_iva", true,  260000000, OMAP3630_VDD_MPU_OPP50_UV),
