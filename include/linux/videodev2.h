b581bea172a7caf757c65998794d97e880c7557b
Danny Baumann <dannybaumann@web.de>
2012-12-13 11:52:31 +0100

    Shuffle ioctl numbers around to keep bionic header compatibility.
    Change-Id: I8d2ac8b0c74a3c06f4f740d5f0b971efbd3fe3bb

--- include/linux/videodev2.h
+++ include/linux/videodev2.h
@@ -913,11 +913,11 @@ enum v4l2_colorfx {
 	V4L2_COLORFX_BW		= 1,
 	V4L2_COLORFX_SEPIA	= 2,
 };
-#define V4L2_CID_AUTOBRIGHTNESS			(V4L2_CID_BASE+32)
-#define V4L2_CID_BAND_STOP_FILTER		(V4L2_CID_BASE+33)
+#define V4L2_CID_AUTOBRIGHTNESS			(V4L2_CID_BASE+34)
+#define V4L2_CID_BAND_STOP_FILTER		(V4L2_CID_BASE+35)
 
-#define V4L2_CID_ROTATE                     	(V4L2_CID_BASE+34)
-#define V4L2_CID_BG_COLOR                       (V4L2_CID_BASE+35)
+#define V4L2_CID_ROTATE                     	(V4L2_CID_BASE+32)
+#define V4L2_CID_BG_COLOR                       (V4L2_CID_BASE+33)
 /* last CID + 1 */
 #define V4L2_CID_LASTP1                         (V4L2_CID_BASE+38)
 
