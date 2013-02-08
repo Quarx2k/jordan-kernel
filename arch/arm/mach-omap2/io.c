196236de6fe37338ac2a9d8c6c2b99dc360904ac
Quarx2k <agent00791@gmail.com>
2013-02-08 19:45:05 +0700

    Revert "Set SDRC clock to 200mhz"
    This reverts commit 89a99d7de0b53ad83d05b17ce0ee7a13ddf2556c.

--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -324,7 +324,7 @@ static int __init _omap2_init_reprogram_sdrc(void)
 
 	rate = clk_get_rate(dpll3_m2_ck);
 	pr_info("Reprogramming SDRC clock to %ld Hz\n", rate);
-	v = clk_set_rate(dpll3_m2_ck, /*rate*/200000000); //TODO: Fix in hboot.
+	v = clk_set_rate(dpll3_m2_ck, rate);
 	if (v)
 		pr_err("dpll3_m2_clk rate change failed: %d\n", v);
 
