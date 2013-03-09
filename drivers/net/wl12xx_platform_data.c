1d114ba9490a02af8e3c528986f62a3131b47b4e
Quarx2k <agent00791@gmail.com>
2013-03-09 17:34:38 +0700

    fix wl12xx platform data

--- drivers/net/wl12xx_platform_data.c
+++ drivers/net/wl12xx_platform_data.c
@@ -1,33 +1,8 @@
-/*
- * This file is part of wl12xx
- *
- * Copyright (C) 2010-2011 Texas Instruments, Inc.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA
- *
- */
-
 #include <linux/module.h>
 #include <linux/err.h>
-//#include <linux/wl12xx.h>
+#include <linux/wl12xx.h>
 
-struct wl12xx_platform_data *wl12xx_get_platform_data(void);
-int wl12xx_set_platform_data(const struct wl12xx_platform_data *data);
-
-static struct wl12xx_platform_data *platform_data;
-
+static const struct wl12xx_platform_data *platform_data;
 
 int __init wl12xx_set_platform_data(const struct wl12xx_platform_data *data)
 {
@@ -36,14 +11,14 @@ int __init wl12xx_set_platform_data(const struct wl12xx_platform_data *data)
 	if (!data)
 		return -EINVAL;
 
-	platform_data = kmemdup(data, sizeof(data), GFP_KERNEL);
+	platform_data = kmemdup(data, sizeof(*data), GFP_KERNEL);
 	if (!platform_data)
 		return -ENOMEM;
 
 	return 0;
 }
 
-struct wl12xx_platform_data *wl12xx_get_platform_data(void)
+const struct wl12xx_platform_data *wl12xx_get_platform_data(void)
 {
 	if (!platform_data)
 		return ERR_PTR(-ENODEV);
