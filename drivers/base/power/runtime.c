4d94493687b964e9554a889a87e3ce09ef654bfa
Quarx2k <agent00791@gmail.com>
2013-03-21 14:36:57 +0700

    PM: allow for usage_count > 0 in pm_runtime_get()

--- drivers/base/power/runtime.c
+++ drivers/base/power/runtime.c
@@ -696,17 +696,23 @@ EXPORT_SYMBOL_GPL(pm_request_resume);
  * @dev: Device to handle.
  * @sync: If set and the device is suspended, resume it synchronously.
  *
- * Increment the usage count of the device and if it was zero previously,
- * resume it or submit a resume request for it, depending on the value of @sync.
- */
++ * Increment the usage count of the device.  If @sync is set, resume the device
++ * and wait for the resume to complete.  Otherwise if the device is currently
++ * suspending or suspended, submit a resume request.
++ *
++ * If @sync is clear, the caller is responsible for synchronization.
+  */
 int __pm_runtime_get(struct device *dev, bool sync)
-{
-	int retval = 1;
+ {
+	int retval = 0;
 
-	if (atomic_add_return(1, &dev->power.usage_count) == 1)
-		retval = sync ? pm_runtime_resume(dev) : pm_request_resume(dev);
-
-	return retval;
+		atomic_inc(&dev->power.usage_count);
+	if (sync)
+		retval = pm_runtime_resume(dev);
+	else if (dev->power.runtime_status == RPM_SUSPENDING ||
+	    dev->power.runtime_status == RPM_SUSPENDED)
+		retval = pm_request_resume(dev);
+ 	return retval;
 }
 EXPORT_SYMBOL_GPL(__pm_runtime_get);
 
