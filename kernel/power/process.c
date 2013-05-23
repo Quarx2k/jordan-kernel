c0eabc28bb9438db7378c66e45446b667fa357f4
Quarx2k <agent00791@gmail.com>
2013-05-23 11:40:50 +0700

    Fixes
    Conflicts:
    	kernel/power/process.c

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -14,7 +14,6 @@
 #include <linux/module.h>
 #include <linux/syscalls.h>
 #include <linux/freezer.h>
-#include <linux/wakelock.h>
 #include <linux/delay.h>
 
 /* 
@@ -39,7 +38,6 @@ static int try_to_freeze_tasks(bool sig_only)
 	struct timeval start, end;
 	u64 elapsed_csecs64;
 	unsigned int elapsed_csecs;
-	unsigned int wakeup = 0;
 
 	do_gettimeofday(&start);
 
@@ -65,11 +63,7 @@ static int try_to_freeze_tasks(bool sig_only)
 				todo++;
 		} while_each_thread(g, p);
 		read_unlock(&tasklist_lock);
-		yield();			/* Yield is okay here */
-		if (todo && has_wake_lock(WAKE_LOCK_SUSPEND)) {
-			wakeup = 1;
-			break;
-		}
+
 		if (!todo || time_after(jiffies, end_time))
 			break;
 
@@ -92,17 +86,14 @@ static int try_to_freeze_tasks(bool sig_only)
 		 * but it cleans up leftover PF_FREEZE requests.
 		 */
 		printk("\n");
-		printk(KERN_ERR "Freezing of tasks %s after %d.%02d seconds "
+		printk(KERN_ERR "Freezing of tasks failed after %d.%02d seconds "
 				"(%d tasks refusing to freeze):\n",
-				wakeup ? "aborted" : "failed",
 				elapsed_csecs / 100, elapsed_csecs % 100, todo);
-		if(!wakeup)
-			show_state();
+		show_state();
 		read_lock(&tasklist_lock);
 		do_each_thread(g, p) {
 			task_lock(p);
-			if (freezing(p) && !freezer_should_skip(p) &&
-							elapsed_csecs > 100)
+			if (freezing(p) && !freezer_should_skip(p))
 				printk(KERN_ERR " %s\n", p->comm);
 			cancel_freezing(p);
 			task_unlock(p);
@@ -173,4 +164,3 @@ void thaw_processes(void)
 	schedule();
 	printk("done.\n");
 }
-
