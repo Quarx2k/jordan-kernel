c7ffbbfdaceb95bf148544d560674d2b591df835
Blechd0se <alex.christ@hotmail.de>
2013-05-25 23:12:16 +0200

    block: various I/O-Tweaks
    Change some default values of our schedulers.
    
    Deadline should now perform more like FiFo.
    Change vm_readahead to 512.
    
    Change-Id: If16227f534362c0e506a3d920c574fb5b1d06e4f
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -17,7 +17,7 @@
  * tunables
  */
 /* max queue in one round of service */
-static const int cfq_quantum = 4;
+static const int cfq_quantum = 8;
 static const int cfq_fifo_expire[2] = { HZ / 4, HZ / 8 };
 /* maximum backwards seek, in KiB */
 static const int cfq_back_max = 16 * 1024;
--- block/sio-iosched.c
+++ block/sio-iosched.c
@@ -23,14 +23,14 @@
 enum { ASYNC, SYNC };
 
 /* Tunables */
-static const int sync_read_expire  = HZ / 2;	/* max time before a sync read is submitted. */
-static const int sync_write_expire = 2 * HZ;	/* max time before a sync write is submitted. */
+static const int sync_read_expire  = 25;	/* max time before a sync read is submitted. */
+static const int sync_write_expire = 250;	/* max time before a sync write is submitted. */
 
-static const int async_read_expire  =  4 * HZ;	/* ditto for async, these limits are SOFT! */
-static const int async_write_expire = 16 * HZ;	/* ditto for async, these limits are SOFT! */
+static const int async_read_expire  =  50;	/* ditto for async, these limits are SOFT! */
+static const int async_write_expire = 500;	/* ditto for async, these limits are SOFT! */
 
 static const int writes_starved = 2;		/* max times reads can starve a write */
-static const int fifo_batch     = 8;		/* # of sequential requests treated as one
+static const int fifo_batch     = 16;		/* # of sequential requests treated as one
 						   by the above parameters. For throughput. */
 
 /* Elevator data */
