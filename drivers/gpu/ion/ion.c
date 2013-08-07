2230550dcc70e5d6bdb3b70490106d99e1603d53
Blechd0se <alex.christ@hotmail.de>
2013-08-07 22:49:27 +0200

    ion: fix memory leak and race condition
    Previous commit introduced a memory leak.
    kfree(client->name) takes care of that.
    
    Otherwise fix a race condition if there are two
    threads in one client and both try to link to the
    same handle.
    
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- drivers/gpu/ion/ion.c
+++ drivers/gpu/ion/ion.c
@@ -370,12 +370,13 @@ void ion_free(struct ion_client *client, struct ion_handle *handle)
 
 	mutex_lock(&client->lock);
 	valid_handle = ion_handle_validate(client, handle);
-	mutex_unlock(&client->lock);
 
 	if (!valid_handle) {
+		mutex_unlock(&client->lock);
 		WARN("%s: invalid handle passed to free.\n", __func__);
 		return;
 	}
+	mutex_unlock(&client->lock);
 	ion_handle_put(handle);
 }
 EXPORT_SYMBOL(ion_free);
@@ -802,6 +803,7 @@ static void _ion_client_destroy(struct kref *kref)
 	debugfs_remove_recursive(client->debug_root);
 	mutex_unlock(&dev->lock);
 
+	kfree(client->name);
 	kfree(client);
 }
 
