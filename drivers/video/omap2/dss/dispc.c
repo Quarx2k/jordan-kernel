a7c922c46db42dd5bd7d606b9d9e5c83e0fc5f76
Quarx2k <agent00791@gmail.com>
2013-11-03 04:32:22 +0700

    Finish revert commit 9281e152e94bbfb44f1, first part reverted by 806aabed6ea907

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -42,7 +42,6 @@
 #include <mach/tiler.h>
 #include <plat/omap-pm.h>
 #include <video/omapdss.h>
-#include <../mach-omap2/powerdomain.h>
 
 #include "../clockdomain.h"
 #include "dss.h"
@@ -571,7 +570,6 @@ err_dss_get:
 
 void dispc_runtime_put(void)
 {
-	struct powerdomain *dss_powerdomain = pwrdm_lookup("dss_pwrdm");
 	mutex_lock(&dispc.runtime_lock);
 
 	if (--dispc.runtime_count == 0) {
@@ -580,14 +578,6 @@ void dispc_runtime_put(void)
 		DSSDBG("dispc_runtime_put\n");
 
 		dispc_save_context();
-
-		/* Sets DSS max latency constraint
-		 * * (allowing for deeper power state)
-		 * */
-		omap_pm_set_max_dev_wakeup_lat(
-				&dispc.pdev->dev,
-				&dispc.pdev->dev,
-				dss_powerdomain->wakeup_lat[PWRDM_FUNC_PWRST_OFF]);
 
 		r = pm_runtime_put_sync(&dispc.pdev->dev);
 		WARN_ON(r);
