65ee25c5ee94f30b29462475f4c592a039f855d7
Steven Luo <steven@steven676.net>
2013-12-07 02:11:20 -0800

    tidspbridge: fix last patch to map same region of physical memory as before
    Commit 559c71fe5dc3 ("Staging: TIDSPBRIDGE: Use vm_iomap_memory for
    mmap-ing instead of remap_pfn_range") had the effect of inadvertently
    shifting the start of the physical memory area mapped by
    pdata->phys_mempool_base.  Correct this by subtracting that shift before
    calling vm_iomap_memory() and adding it back afterwards.
    
    Reported-by: Dheeraj CVR <cvr.dheeraj@gmail.com>
    Signed-off-by: Steven Luo <steven@steven676.net>

--- drivers/staging/tidspbridge/rmgr/drv_interface.c
+++ drivers/staging/tidspbridge/rmgr/drv_interface.c
@@ -624,6 +624,8 @@ err:
 static int bridge_mmap(struct file *filp, struct vm_area_struct *vma)
 {
 	u32 offset = vma->vm_pgoff << PAGE_SHIFT;
+	unsigned long base_pgoff;
+	int status;
 
 	struct omap_dsp_platform_data *pdata =
 	    omap_dspbridge_dev->dev.platform_data;
@@ -637,9 +639,29 @@ static int bridge_mmap(struct file *filp, struct vm_area_struct *vma)
 		"%lx flags %lx\n", __func__, filp, offset,
 		vma->vm_start, vma->vm_end, vma->vm_page_prot, vma->vm_flags);
 
-	return vm_iomap_memory(vma,
-			       pdata->phys_mempool_base,
-			       pdata->phys_mempool_size);
+	/*
+	 * vm_iomap_memory() expects vma->vm_pgoff to be expressed as an offset
+	 * from the start of the physical memory pool, but we're called with
+	 * a pfn (physical page number) stored there instead.
+	 *
+	 * To avoid duplicating lots of tricky overflow checking logic,
+	 * temporarily convert vma->vm_pgoff to the offset vm_iomap_memory()
+	 * expects, but restore the original value once the mapping has been
+	 * created.
+	 */
+	base_pgoff = pdata->phys_mempool_base >> PAGE_SHIFT;
+	if (vma->vm_pgoff < base_pgoff)
+		return -EINVAL;
+	vma->vm_pgoff -= base_pgoff;
+
+	status = vm_iomap_memory(vma,
+				 pdata->phys_mempool_base,
+				 pdata->phys_mempool_size);
+
+	/* Restore the original value of vma->vm_pgoff */
+	vma->vm_pgoff += base_pgoff;
+
+	return status;
 }
 
 /* To remove all process resources before removing the process from the
