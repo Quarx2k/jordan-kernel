5da3850431bf5938206076fcc86640fa82a0b763
Quarx2k <agent00791@gmail.com>
2014-01-28 20:27:03 +0700

    Revert "add last_kmsg support, not sure if that works"
    This reverts commit 4a12cb8bb19505edbac06aef1f3dc6fde587fc42.

--- arch/arm/mach-omap2/board-mapphone.c
+++ arch/arm/mach-omap2/board-mapphone.c
@@ -75,7 +75,6 @@
 #include "cm.h"
 #include "clock.h"
 #include "omap_ion.h"
-#include "omap_ram_console.h"
 
 #ifdef CONFIG_VIDEO_OMAP3
 #include <media/v4l2-int-device.h>
@@ -124,12 +123,6 @@
 #define I2C_BUS_MAX_DEVICES 5
 #define I2C_MAX_DEV_NAME_LEN 16
 #define I2C_BUS_PROP_NAME_LEN 12
-
-#ifdef CONFIG_OMAP_RAM_CONSOLE
-#define MAPPHONE_RAM_CONSOLE_START  PHYS_OFFSET + 0xE000000
-/* XXX: size is ridiculously large, but system won't boot with smaller */
-#define MAPPHONE_RAM_CONSOLE_SIZE    SZ_1M
-#endif
 
 char *bp_model = "UMTS";
 
@@ -1758,10 +1751,6 @@ static void __init mapphone_init(void)
 
 static void __init mapphone_reserve(void)
 {
-#ifdef CONFIG_OMAP_RAM_CONSOLE
-        omap_ram_console_init(MAPPHONE_RAM_CONSOLE_START,
-                        MAPPHONE_RAM_CONSOLE_SIZE);
-#endif
 	omap_reserve();
 #ifdef CONFIG_ION_OMAP
 	omap_ion_init();
