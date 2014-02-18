75bfdec6c291e9857e9cfdee1eee1e216201949b
Quarx2k <agent00791@gmail.com>
2014-02-18 19:40:33 +0700

    fix battery status

--- drivers/power/cpcap-battery.c
+++ drivers/power/cpcap-battery.c
@@ -467,7 +467,7 @@ static int cpcap_batt_get_property(struct power_supply *psy,
 
 	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
 #ifdef USE_OWN_CALCULATE_METHOD
-		val->intval = (cpcap_batt_value(sply, CPCAP_ADC_BATTP)*1000));
+		val->intval = cpcap_batt_value(sply, CPCAP_ADC_BATTP)*1000;
 #else
 		val->intval = sply->batt_state.batt_volt;
 #endif
@@ -504,9 +504,10 @@ static int cpcap_batt_get_property(struct power_supply *psy,
 
 static int cpcap_batt_status(struct cpcap_batt_ps *sply) {
         if (sply->usb_state.online == 1 || sply->ac_state.online == 1) {
-	   return POWER_SUPPLY_STATUS_CHARGING;
-        } else if (cpcap_batt_counter(sply) > 95) {
-	   return POWER_SUPPLY_STATUS_FULL;
+		if (cpcap_batt_counter(sply) > 95)
+			return POWER_SUPPLY_STATUS_FULL;
+		else
+			return POWER_SUPPLY_STATUS_CHARGING;
         } else {
            return POWER_SUPPLY_STATUS_DISCHARGING;
 	}
