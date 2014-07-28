762295046a8a2a15c498f63feada5f3b62d69f98
Blechd0se <alex.christ@hotmail.de>
2014-07-28 21:30:55 +0200

    input: touchboost: merge touchboost work in
    Directly based upon the work here; https://github.com/Blechd0se/kernel-moto-g/commit/777ac1216bad4820a1c1f7ac78dc27aac37a37a6
    This might also fix a very rare kernel panic.
    
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- drivers/input/touchscreen/touchboost.c
+++ drivers/input/touchscreen/touchboost.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Francisco Franco. All rights reserved.
+ * Copyright (c) 2013-2014, Francisco Franco. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -17,6 +17,7 @@
 #include <linux/init.h>
 #include <linux/notifier.h>
 #include <linux/cpufreq.h>
+#include <linux/cpu.h>
 #include <linux/sched.h>
 #include <linux/jiffies.h>
 #include <linux/kthread.h>
@@ -25,13 +26,7 @@
 #include <linux/input.h>
 #include <linux/time.h>
 
-#define MIM_TIME_INTERVAL_US (100 * USEC_PER_MSEC)
-
-/* 
- * This variable comes from interactive governor. If you want to implement this
- * driver you have to also implement this variable on your governor of choice 
- */
-extern int input_boost_freq;
+#define MIM_TIME_INTERVAL_US (150 * USEC_PER_MSEC)
 
 /*
  * Use this variable in your governor of choice to calculate when the cpufreq
@@ -40,80 +35,10 @@ extern int input_boost_freq;
  */
 u64 last_input_time;
 
-int boost_freq_buf;
-
-static struct workqueue_struct *input_boost_wq;
-static struct work_struct input_boost_work;
-static struct delayed_work rem_input_boost;
-
-/*
- * The CPUFREQ_ADJUST notifier is used to override the current policy min to
- * make sure policy min >= boost_min. The cpufreq framework then does the job
- * of enforcing the new policy.
- */
-static int boost_adjust_notify(struct notifier_block *nb, unsigned long val, void *data)
-{
-	struct cpufreq_policy *policy = data;
-	unsigned int cpu = policy->cpu;
-
-	if (val != CPUFREQ_ADJUST)
-		return NOTIFY_OK;
-
-	/* just in case someone underclocks below input_boost_freq */
-	if (boost_freq_buf > policy->max)
-		boost_freq_buf = policy->max;
-
-	pr_debug("CPU%u policy min before boost: %u kHz\n",
-		 cpu, policy->min);
-	pr_debug("CPU%u boost min: %u kHz\n", cpu, boost_freq_buf);
-
-	cpufreq_verify_within_limits(policy, boost_freq_buf, UINT_MAX);
-
-	pr_debug("CPU%u policy min after boost: %u kHz\n",
-		 cpu, policy->min);
-
-	return NOTIFY_OK;
-}
-
-static struct notifier_block boost_adjust_nb = {
-	.notifier_call = boost_adjust_notify,
-};
-
-static void do_rem_input_boost(struct work_struct *work)
-{
-	unsigned int i;
-	boost_freq_buf = 0;
-	/* Force policy re-evaluation to trigger adjust notifier. */
-	for_each_online_cpu(i)
-		cpufreq_update_policy(i);
-}
-
-static void do_input_boost(struct work_struct *work)
-{
-	unsigned int i, ret;
-	struct cpufreq_policy policy;
-
-	/* 
-	 * to avoid concurrency issues we cancel rem_input_boost
-	 * and wait for it to finish the work
-	 */
-	cancel_delayed_work_sync(&rem_input_boost);
-
-	boost_freq_buf = input_boost_freq;
-
-	for_each_online_cpu(i)
-	{
-		ret = cpufreq_get_policy(&policy, i);
-		if (ret)
-			continue;
-		
-		if (policy.cur >= input_boost_freq)
-			continue;
-
-		cpufreq_update_policy(i);
-	}
-	queue_delayed_work_on(0, input_boost_wq, &rem_input_boost, msecs_to_jiffies(40));
-}
+struct touchboost_inputopen {
+	struct input_handle *handle;
+	struct work_struct inputopen_work;
+} touchboost_inputopen;
 
 static void boost_input_event(struct input_handle *handle,
                 unsigned int type, unsigned int code, int value)
@@ -125,11 +50,19 @@ static void boost_input_event(struct input_handle *handle,
 	if (now - last_input_time < MIM_TIME_INTERVAL_US)
 		return;
 
-	if (work_pending(&input_boost_work))
-		return;
-
-	queue_work_on(0, input_boost_wq, &input_boost_work);
 	last_input_time = ktime_to_us(ktime_get());
+}
+
+static void boost_input_open(struct work_struct *w)
+{
+	struct touchboost_inputopen *io = 
+		container_of(w, struct touchboost_inputopen, inputopen_work);
+
+	int error;
+
+	error = input_open_device(io->handle);
+	if (error)
+		input_unregister_handle(io->handle);
 }
 
 static int boost_input_connect(struct input_handler *handler,
@@ -144,20 +77,17 @@ static int boost_input_connect(struct input_handler *handler,
 
 	handle->dev = dev;
 	handle->handler = handler;
-	handle->name = "cpufreq";
+	handle->name = "touchboost";
 
 	error = input_register_handle(handle);
 	if (error)
-		goto err2;
+		goto err;
 
-	error = input_open_device(handle);
-	if (error)
-		goto err1;
-
+	touchboost_inputopen.handle = handle;
+	schedule_work(&touchboost_inputopen.inputopen_work);
 	return 0;
-err1:
-	input_unregister_handle(handle);
-err2:
+
+err:
 	kfree(handle);
 	return error;
 }
@@ -173,11 +103,19 @@ static const struct input_device_id boost_ids[] = {
 	/* multi-touch touchscreen */
 	{
 		.flags = INPUT_DEVICE_ID_MATCH_EVBIT |
-				INPUT_DEVICE_ID_MATCH_ABSBIT,
+			INPUT_DEVICE_ID_MATCH_ABSBIT,
 		.evbit = { BIT_MASK(EV_ABS) },
 		.absbit = { [BIT_WORD(ABS_MT_POSITION_X)] =
-				BIT_MASK(ABS_MT_POSITION_X) |
-				BIT_MASK(ABS_MT_POSITION_Y) },
+			BIT_MASK(ABS_MT_POSITION_X) |
+			BIT_MASK(ABS_MT_POSITION_Y) },
+	},
+	/* touchpad */
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_KEYBIT |
+			INPUT_DEVICE_ID_MATCH_ABSBIT,
+		.keybit = { [BIT_WORD(BTN_TOUCH)] = BIT_MASK(BTN_TOUCH) },
+		.absbit = { [BIT_WORD(ABS_X)] =
+			BIT_MASK(ABS_X) | BIT_MASK(ABS_Y) },
 	},
 	/* Keypad */
 	{
@@ -198,22 +136,10 @@ static struct input_handler boost_input_handler = {
 static int init(void)
 {
 	int ret;
-	input_boost_wq = create_singlethread_workqueue("input_boost_wq");
-
-	if (!input_boost_wq)
-		return -ENOMEM;
-
-	INIT_WORK(&input_boost_work, do_input_boost);
-	INIT_DELAYED_WORK(&rem_input_boost, do_rem_input_boost);
+	INIT_WORK(&touchboost_inputopen.inputopen_work, boost_input_open);
 
 	ret = input_register_handler(&boost_input_handler);
-	if (ret) {
-		pr_err("Failed to register input handler, error: %d", ret);
-		return ret;
-	}
 
-	cpufreq_register_notifier(&boost_adjust_nb, CPUFREQ_POLICY_NOTIFIER);
-
-	return 0;
+	return ret;
 }
 late_initcall(init);
