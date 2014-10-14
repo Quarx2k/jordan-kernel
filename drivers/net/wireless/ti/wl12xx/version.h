6214ded73c64bbbbaee0271eabf49b870511b292
Quarx2k <agent00791@gmail.com>
2014-10-14 16:50:11 +0700

    Cleanup devtree

--- drivers/net/wireless/ti/wl12xx/version.h
+++ drivers/net/wireless/ti/wl12xx/version.h
@@ -1,1 +1,1 @@
-static const char *wl12xx_git_head = "android-4.4w_r4-16-g8633651-dirty";
+static const char *wl12xx_git_head = "android-4.4w_r4-17-g2b24990";
--- drivers/net/wireless/ti/wlcore/version.h
+++ drivers/net/wireless/ti/wlcore/version.h
@@ -1,2 +1,2 @@
 static const char *wlcore_timestamp = __TIMESTAMP__;
-static const char *wlcore_git_head = "android-4.4w_r4-16-g8633651-dirty";
+static const char *wlcore_git_head = "android-4.4w_r4-17-g2b24990";
